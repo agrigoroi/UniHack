1b59b648910988daaece58f5793336350492e9d7
Alexandru Grigoroi <alexandru@grigoroi.com>
2014-03-08 16:04:20 +0000

    Fixed something

--- app/main/Game.java
+++ app/main/Game.java
@@ -14,10 +14,28 @@ public class Game {
     public static ArrayList<LatLng> locations = new ArrayList<>();
     public HashMap<String, LatLng> guesses = new HashMap<>();
 
-
     public Game(LatLng center) {
         this.center = center;
         this.endTime = new Date((new Date()).getTime() + 60*1000);
+
+        Timer timer = new Timer();
+        TimerTask endGame = new TimerTask() {
+            public void run()
+            {
+                for(String player: Global.currentGame.guesses.keySet()) {
+                    LatLng guess = Global.currentGame.guesses.get(player);
+                    System.out.println(player + ": " + guess.getX() + ", " + guess.getY());
+                }
+            }
+        };
+        timer.schedule(endGame, endTime.getTime() - (new Date()).getTime());
+        TimerTask newGame = new TimerTask() {
+            public void run()
+            {
+                Global.currentGame = new Game();
+            }
+        };
+        timer.schedule(newGame, endTime.getTime() - (new Date()).getTime() + 10*1000);
     }
 
     public Game() {
