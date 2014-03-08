384dca6045a29f1ed4eeb443e7d5f479a04bf2a4
Alexandru Grigoroi <alexandru@grigoroi.com>
2014-03-08 12:50:12 +0000

    Street View Showing

---
+++ public/javascripts/main.js
@@ -0,0 +1,140 @@
+var svp;
+
+var initialize = function() {
+
+  var cookies = {
+    set: function(name,value,days) {
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+      } else {
+        var expires = "";
+      }
+      document.cookie = name+"="+value+expires+"; path=/";
+    },
+
+    read: function(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      }
+      return null;
+    },
+
+    erase: function(name) {
+      createCookie(name,"",-1);
+    }
+  }
+
+  var pinColor = "00FF00";
+  var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0,0),
+      new google.maps.Point(10, 34));
+  var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+      new google.maps.Size(40, 37),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(12, 35));
+  pinColor = "0000FF";
+  var pinLocationImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0,0),
+    new google.maps.Point(10, 34));
+  var pinLocationShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+    new google.maps.Size(40, 37),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(12, 35));
+
+  var user_info = null;
+  var end_time = null;
+  var next_round = null;
+  var guesses = {};
+  var timeLeftDiv = document.getElementById("timeleft");
+
+  svp = new google.maps.StreetViewPanorama(document.getElementById("streetview"), {
+    addressControl: false,
+    linksControl: true,
+    position: new google.maps.LatLng(53.474, -2.248),
+    visible: false,
+    pov: {
+      heading: Math.floor(Math.random()*360),
+      pitch: +5
+    }
+  });
+  svp.controls[google.maps.ControlPosition.RIGHT_TOP].push(document.getElementById("mycontrol"));
+
+  svp.setVisible(true);
+
+  var map = new google.maps.Map(document.getElementById("smallmap"), {
+    zoom: 0,
+    center: new google.maps.LatLng(53.474, -2.248),
+    keyboardShortcuts: false,
+    streetViewControl: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var guessMarker = null;
+
+  if(guessMarker !== null) {
+    guessMarker.setVisible = false;
+    guessMarker.setMap(null);
+  }
+
+  google.maps.event.addListener(map, 'click', function(event) {
+    makeGuess(event.latLng);
+  });
+
+  function makeGuess(place) {
+    if(guessMarker !== null) {
+      guessMarker.setVisible = false;
+      guessMarker.setMap(null);
+    }
+    guessMarker = new google.maps.Marker({
+      position: place,
+      map: map,
+//      title: user_info.name,
+      clickable: false,
+      icon: pinImage,
+      shadow: pinShadow
+    });
+  }
+
+//  function updateTime() {
+//    if(end_time !== null) {
+//      if(moment().isBefore(end_time)) {
+//        timeLeftDiv.innerHTML = "Time left: " + end_time.diff(moment(), "seconds") + " seconds";
+//      } else {
+//        timeLeftDiv.innerHTML = "Finished.";
+//        if(next_round !== null) {
+//          timeLeftDiv.innerHTML = timeLeftDiv.innerHTML + " Next rount starts in " + next_round.diff(moment(), "seconds") + " seconds";
+//        }
+//      }
+//    } else {
+//      timeLeftDiv.innerHTML = "Time left: unknown"
+//    }
+//  }
+//
+//  function min(x, y) {
+//    if(x<y)
+//      return x;
+//    else
+//      return y;
+//  }
+
+  // document.getElementById('OpenTheModal').click();
+}
+
+window.onload = function() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyB-9UdzjbK_V2-2W5HaF-j26eph2woqkCM&sensor=false&callback=initialize';
+  document.body.appendChild(script);
+  script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "assets/javascripts/modal.js";
+  document.body.appendChild(script);
+}
