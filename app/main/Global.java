89ad45cd62f94351e70e0c1d9570bc34f1f6e7e7
Alexandru Grigoroi <alexandru@grigoroi.com>
2014-03-08 15:19:57 +0000

    Fixed coordonates

--- app/main/Global.java
+++ app/main/Global.java
@@ -14,18 +14,11 @@ public class Global extends GlobalSettings {
 
     @Override
     public void onStart(Application app) {
-        System.out.println("Here");
-        Game.locations.add(new LatLng(0, 0));
         Game.locations.add(new LatLng(53.474, -2.248));
-        System.out.println("Here");
+        Game.locations.add(new LatLng(53.474, -2.248));
         currentGame = new Game();
     }
     @Override
     public void onStop(Application app) {
-        System.out.println("Here");
-        Game.locations.add(new LatLng(53.474, -2.248));
-        Game.locations.add(new LatLng(53.474, -2.248));
-        System.out.println("Here");
-        currentGame = new Game();
     }
 }
